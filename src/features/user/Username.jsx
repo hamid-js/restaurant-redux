d851998e5a13b575ee62a1564bee0aa0df2d4ce4
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2023-12-18 18:19:33 +0330

    edit some bugs2

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -2,9 +2,12 @@ import { useSelector } from 'react-redux';
 
 function Username() {
   const username = useSelector((state) => state.user.username);
- 
+
   if (!username) return null;
-  return <div className="hidden text-sm font-semibold md:block">{username}</div>;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
