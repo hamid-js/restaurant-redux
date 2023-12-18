34ab14dc2767812d4aeae8194a391ca0ece4d43a
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2023-12-18 18:44:26 +0330

    edit home page ui

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -6,14 +6,13 @@ function Home() {
   const username = useSelector((state) => state.user.username);
   return (
     <div className="my-14  text-center">
-      <h1 className="mb-10 text-center text-xl font-semibold text-gray-500">
+      <h1 className="mb-7 text-center text-xl font-semibold text-gray-500">
       رستوران خوش طعم
-        <br />
         
       </h1>
-      <div className="text-orange-500 mb-16 text-xl font-semibold   ">
+      <h2 className="text-orange-500 mb-24 text-xl font-semibold   ">
         احساس بهترین طعم با رستوران خوش طعم
-        </div>
+        </h2>
    { username === "" ? (<CreateUser />) :(<Button to="menu" type="primary" >
     ادامه سفارش {username}
    </Button>)
