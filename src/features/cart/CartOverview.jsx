01e6c05277ff3c298143a6a5c1ada822b12411a9
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2024-04-02 20:10:37 +0330

    update responsive

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -8,13 +8,13 @@ function CartOverview() {
 
   if (!totalCartPrice) return null;
   return (
-    <div className="flex flex-row-reverse items-center justify-between bg-stone-800 p-4 uppercase text-stone-200  ">
-      <p className="space-x-8 font-semibold text-stone-300">
-        <span> تعداد {totalCartQuantity}</span>
+    <div className="flex flex-row-reverse md:w-4/5 md:mx-auto md:my-5 md:rounded-xl items-center justify-between bg-stone-800 p-4 uppercase text-stone-200 md:py-5 md:text-xl  md:px-16  ">
+      <p className="space-x-8 md:space-x-32 font-semibold text-stone-300">
+        <span  className='md:font-bold md:px-16 md:text-orange-300'> تعداد {totalCartQuantity}</span>
         <span> </span>
-        <span>مبلغ کل : ${totalCartPrice}</span>
+        <span  className='md:font-bold  md:text-orange-400'>مبلغ کل : ${totalCartPrice}</span>
       </p>
-      <Link to="/cart"> سبد خرید &rarr;</Link>
+      <Link className='md:bg-orange-400 md:p-2 md:rounded-lg md:text-black' to="/cart"> سبد خرید &rarr;</Link>
     </div>
   );
 }
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -8,8 +8,8 @@ import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch();
-  const currentQuantity = useSelector(getCurrentQuantityById(id))
-const isInQuantity = currentQuantity >= 1
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInQuantity = currentQuantity >= 1;
   function handleAddToCart() {
     const newItem = {
       pizzaId: id,
@@ -21,34 +21,34 @@ const isInQuantity = currentQuantity >= 1
     dispatch(addItem(newItem));
   }
   return (
-    <li className="flex gap-4 py-2">
+    <li className="items-center justify-between flex gap-20 py-2">
       <img
         src={imageUrl}
         alt={name}
-        className={`h-24 ${soldOut ? 'opacity-70 grayscale ' : ''}`}
+        className={`h-24  ${soldOut ? 'opacity-70 grayscale ' : ''}`}
       />
-      <div className="felx-col flex grow pt-0.5">
+      <div className="flex grow flex-col pt-0.5">
         <p className="font-medium">{name}</p>
         <p className="text-sm capitalize italic text-stone-500">
           {ingredients.join(', ')}
         </p>
-        <div className="mt-auto flex items-center justify-between ">
+        <div className="my-5 flex items-center justify-between mx-6 ">
           {!soldOut ? (
-            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+            <p className="text-xl">{formatCurrency(unitPrice)}</p>
           ) : (
-            <p className="text-sm font-medium uppercase text-stone-500">
+            <p className="text-xl font-medium uppercase text-stone-800">
               ناموجود
             </p>
           )}
           {isInQuantity && (
-            <div className='flex items-center gap-3 sm:gap-8'>
-              <UpdateItemQuantity  pizzaId={id}/>
-            <DeleteItem pizzaId={id}/>
-          </div>
-            ) }
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity pizzaId={id} />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
           {!soldOut && !isInQuantity && (
             <Button type="small" onClick={handleAddToCart}>
-         افزودن 
+              افزودن
             </Button>
           )}
         </div>
